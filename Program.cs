f778f0cca4cc4f065d5a980f12811b85f7698348
Paulo Henrique <paulo.henrique@infinitsolucoes.com>
2022-10-26 13:38:57 -0300

    Adicionado Cors para testar integraçao

--- Program.cs
+++ Program.cs
@@ -12,6 +12,14 @@ builder.Services.AddDbContextPool<ApiDbContext>(options =>
                       ServerVersion.AutoDetect(mySqlConnection)));
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+        builder.SetIsOriginAllowed(_ => true)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,10 +29,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 //}
-
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
